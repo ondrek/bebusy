eab02b7c688739e77ac68a72dca0dc0f307cc777
Samuel Ondrek <samuel@ondrek.com>
2014-10-21 13:33:59 +0200

    Random Module, Fixed Time, Added Messages

--- bebusy.js
+++ bebusy.js
@@ -21,7 +21,7 @@
     var BeBusy = function(options) {
 
         this.improveConsoleFn()
-        this.askInitQuestions();
+        this.getAuthenticParagraph();
 
         this.options = options ? options : {
             version : options.version || 1,
@@ -51,12 +51,6 @@
 
         var questions = "[bebusy] What speed of generated messages do you want (slow, medium, fast): ";
 
-
-
-        that.getAuthenticParagraph();
-
-
-        return;
         rl.question(questions, function(answer){
             that.generateIpsum();
             rl.close();
@@ -70,10 +64,30 @@
      */
     BeBusy.prototype.getAuthenticParagraph = function() {
         
-        var prefix = "npm";
         var type = ["info", "verb"];
         var that = this;
-        
+
+        var randomMessage = this.getRandomMessage();
+        var randomColor = this.getRandomColor();
+        var randomStatus = this.getRandomStatus();
+        var randomModule = this.getRandomModule();
+
+        console.log(randomModule.grey + " " + randomStatus.magenta + " " + randomMessage);
+
+        setTimeout(function(){
+            that.getAuthenticParagraph();
+        }, 70);
+
+
+    };
+
+
+
+    /**
+     *
+     */
+    BeBusy.prototype.getRandomMessage = function() {
+
         var senteces = [
             "it worked if it ends with ok",
             "cli [ 'node', '/usr/local/bin/npm', 'install', '--verbose' ]",
@@ -99,13 +113,147 @@
             "ok"
         ];
 
-        var randomColor = that.getRandomColor();
-        senteces.forEach(function(one){
+        return underscoreMdl.sample(senteces);
 
-            console.log("npm ".grey + "info ".cyan + one[randomColor]);
-        });
+    };
 
-        this.getAuthenticParagraph();
+
+
+    /**
+     *
+     */
+    BeBusy.prototype.getRandomModule = function() {
+
+        var modules = [
+            "npm",
+            "install",
+            "ruby",
+            "download",
+            "parse",
+            "ok"
+        ];
+
+        return underscoreMdl.sample(modules);
+
+    };
+
+
+
+    /**
+     *
+     */
+    BeBusy.prototype.getRandomStatus = function() {
+
+        var allColors = [
+            "100 Continue",
+            "101 Switching Protocols",
+            "102 Processing",
+            "200 OK",
+            "201 Created",
+            "202 Accepted",
+            "203 Non-Authoritative Information",
+            "204 No Content",
+            "205 Reset Content",
+            "206 Partial Content",
+            "207 Multi-Status",
+            "208 Already Reported",
+            "226 IM Used (RFC 3229)",
+            "300 Multiple Choices",
+            "301 Moved Permanently",
+            "302 Found",
+            "303 See Other",
+            "304 Not Modified",
+            "305 Use Proxy",
+            "306 Switch Proxy",
+            "307 Temporary Redirect",
+            "308 Permanent Redirect",
+            "400 Bad Request",
+            "401 Unauthorized",
+            "402 Payment Required",
+            "403 Forbidden",
+            "404 Not Found",
+            "405 Method Not Allowed",
+            "406 Not Acceptable",
+            "407 Proxy Authentication Required",
+            "408 Request Timeout",
+            "409 Conflict",
+            "410 Gone",
+            "411 Length Required",
+            "412 Precondition Failed",
+            "413 Request Entity Too Large",
+            "414 Request-URI Too Long",
+            "415 Unsupported Media Type",
+            "416 Requested Range Not Satisfiable",
+            "417 Expectation Failed",
+            "418 I'm a teapot",
+            "419 Authentication Timeout",
+            "420 Method Failure",
+            "420 Enhance Your Calm",
+            "422 Unprocessable Entity",
+            "423 Locked",
+            "424 Failed Dependency",
+            "426 Upgrade Required",
+            "428 Precondition Required",
+            "429 Too Many Requests",
+            "431 Request Header Fields Too Large",
+            "440 Login Timeout",
+            "444 No Response",
+            "449 Retry With",
+            "450 Blocked by Windows Parental Controls",
+            "451 Unavailable For Legal Reasons",
+            "451 Redirect",
+            "494 Request Header Too Large",
+            "495 Cert Error",
+            "496 No Cert",
+            "497 HTTP to HTTPS",
+            "498 Token expired/invalid",
+            "499 Client Closed Request",
+            "499 Token required",
+            "500 Internal Server Error",
+            "501 Not Implemented",
+            "502 Bad Gateway",
+            "503 Service Unavailable",
+            "504 Gateway Timeout",
+            "505 HTTP Version Not Supported",
+            "506 Variant Also Negotiates",
+            "507 Insufficient Storage",
+            "508 Loop Detected",
+            "509 Bandwidth Limit Exceeded",
+            "510 Not Extended",
+            "511 Network Authentication Required",
+            "520 Origin Error",
+            "521 Web server is down",
+            "522 Connection timed out",
+            "523 Proxy Declined Request",
+            "524 A timeout occurred",
+            "598 Network read timeout error",
+            "599 Network connect timeout error",
+            "prepublish",
+            "postinstall",
+            "install",
+            "rebuildBundles",
+            "linkMans",
+            "linkBins",
+            "linkStuff",
+            "install",
+            "about to build",
+            "addNamed",
+            "lock",
+            "etag",
+            "parsed url",
+            "search",
+            "query",
+            "host",
+            "auth",
+            "slashes",
+            "cache add",
+            "GET",
+            "POST",
+            "trying",
+            "installOne"
+        ];
+
+        return underscoreMdl.sample(allColors);
 
     };
 
@@ -139,21 +287,33 @@
      */
     BeBusy.prototype.improveConsoleFn = function() {
 
-        var date = new Date();
         var log = console.log;
+        var that = this;
+
+        console.log = function(){
+            log.apply(console, [that.getFormattedDate()].concat(arguments[0]));
+        };
+
+    };
+
+
+    /**
+     *
+     */
+    BeBusy.prototype.getFormattedDate = function() {
+
+        var date = new Date();
 
         var leadingZero = function(nmbr){
             if (nmbr<10) { return ("0" + nmbr) } else { return nmbr }
         };
 
-        var formattedTime = "[bebusy] ";
+        var formattedTime = "";
         formattedTime += leadingZero( date.getHours() );
         formattedTime += ":" + leadingZero( date.getMinutes() );
         formattedTime += ":" + leadingZero( date.getSeconds() );
 
-        console.log = function(){
-            log.apply(console, [formattedTime + " - "].concat(arguments[0]));
-        };
+        return formattedTime;
 
     };
 
